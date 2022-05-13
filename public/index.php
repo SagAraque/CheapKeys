0c923f6bb7b82bcee2e7b94e4e21022baa84a773
Sagaraque <sagaraque@gmail.com>
2022-05-13 14:28:36 +0200

    Index structuration started

--- public/index.php
+++ public/index.php
@@ -1,9 +1,15 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\Cache\Adapter\PhpFilesAdapter;
+use Doctrine\ORM;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
+
+    $cache = new \Symfony\Component\Cache\Adapter\PhpFilesAdapter('doctrine_queries');
+    $config = new \Doctrine\ORM\Configuration();
+    $config->setQueryCache($cache);
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
