853ca40a40287fe4c1b2abadf1c7e79a26fd3e4d
Sagaraque <sagaraque@gmail.com>
2022-06-02 16:59:29 +0200

    Redesigned mobile gallery

--- public/static/javascript/product.js
+++ public/static/javascript/product.js
@@ -1,21 +1,36 @@
 let min = document.querySelectorAll('[class*="gallery__min"]'),
-gallery = document.querySelector('.gallery__asset'),
-arrows = document.querySelectorAll('.gallery__arrow'),
-objectIndex = 0,
-wishlist = document.querySelector('.info__button--wish'),
-wishlistIcon = document.querySelector('.info_icon--wish'),
-cartButton = document.querySelector('.info__button--buy'),
-cartNum = document.querySelector('.header__cartCant');
+    gallery = document.querySelector('.gallery__asset'),
+    arrows = document.querySelectorAll('.gallery__arrow'),
+    objectIndex = 0,
+    wishlist = document.querySelector('.info__button--wish'),
+    wishlistIcon = document.querySelector('.info_icon--wish'),
+    cartButton = document.querySelector('.info__button--buy'),
+    cartNum = document.querySelector('.header__cartCant');
 
+// Mobile gallery variables
+let slider = document.querySelector('.slider'),
+    wrapper = document.querySelector('.gallery__wrapper'),
+    wrapperImg = document.querySelectorAll('.gallery__asset--mobile'),
+    sliderPaginator = document.querySelectorAll('[class*="gallery__ball"]'),
+    wrapperPos = 0;
+
+
+wrapper.addEventListener('dragstart',  (e)=> {
+    wrapGallery(wrapperImg, wrapper, e.pageX, sliderPaginator);
+});
+
+window.addEventListener('resize', ()=>{
+    wrapperPos = 0;
+    wrapper.style.transform = `translateX(0px)`;
+});
 
 // Wishlist button
-
 wishlist.addEventListener('click', ()=>{
    setWishlist(product);
 });
 
-// Gallery buttons
 
+// Gallery buttons
 min.forEach(element => {
     element.addEventListener('click', ()=>{
         if(!element.classList.contains('gallery__min--selected')){
@@ -34,8 +49,8 @@ arrows.forEach(arrow =>{
     });
 });
 
-// Cart button
 
+// Cart button
 cartButton.addEventListener('click', ()=>{
     setGameCart(product);
 });
@@ -43,7 +58,6 @@ cartButton.addEventListener('click', ()=>{
 
 
 // Gallery functions
-
 function changeGalleryImg(min)
 {
     let minSelected = document.querySelector('.gallery__min--selected');
@@ -158,3 +172,32 @@ function setGameCart(gameData)
         }
     }
 }
+
+
+function wrapGallery(imgs, wrapper, initialPos, paginator)
+{
+    wrapper.addEventListener('mousemove', (e)=>{
+        let imgWidth = imgs[0].offsetWidth,
+            max = imgWidth * imgs.length,
+            direction = 0;
+
+        e.pageX > initialPos ? direction = -1 : direction = 1
+
+        wrapperPos -= imgWidth * direction;
+
+        if(Math.abs(wrapperPos) >= max) wrapperPos = 0;
+        if(wrapperPos > 0) wrapperPos = -max + imgWidth;
+
+        wrapper.style.transform = `translateX(${wrapperPos}px)`;
+
+        // Change paginator classes
+        let index = Math.abs(wrapperPos / imgWidth);
+        
+        paginator.forEach(ball => {
+            ball.classList.replace('gallery__ball--selected', 'gallery__ball');
+        });
+        paginator[index].classList.replace('gallery__ball', 'gallery__ball--selected');
+
+    }, {once : true});
+}
+
